47bb0c4a17fb51b5f0d0bc7018264dcc55ef1dc5
thimawtee <timothyketapang@gmail.com>
2026-03-05 00:05:02 +0700

    Menyelesaikan PRAKTIKUM 7 - Custom Validation Error

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,4 @@
-import { Elysia } from "elysia";
+import { Elysia, t } from "elysia";
 import { openapi } from "@elysiajs/openapi";
 
 const app = new Elysia().use(openapi());
@@ -38,6 +38,31 @@ app.get("/product", () => ({
   id: 1,
   name: "Laptop"
 }));
+
+app.onError(({ code, set }) => {
+    if (code === "VALIDATION") {
+      set.status = 400;
+      return {
+        success: false,
+        error: "Validation Error"
+      };
+    }
+  });
+
+app.post(
+  "/login",
+  ({ body }) => {
+    return {
+      message: "Login berhasil"
+    };
+  },
+  {
+    body: t.Object({
+      email: t.String(),
+      password: t.String({ minLength: 8 })
+    })
+  }
+);
 
 app.listen(3000);
 console.log("Server running at http://localhost:3000");
